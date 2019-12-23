e5517509347caf39376255d09c95da91b17ff996
Stiqilis <sarrenabbleoos@gmail.com>
2019-12-23 17:53:36 +0100

    adding spring config. closes #22

--- pet-clinic-data/src/main/java/guru/springframework/services/OwnerService.java
+++
@@ -1,9 +0,0 @@
-package guru.springframework.services;
-
-import guru.springframework.model.Owner;
-
-public interface OwnerService extends CrudService<Owner, Long>{
-
-    Owner findByLastName(String lastName);
-
-}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person extends BaseEntity{
+
+    private String fristName;
+    private String lastName;
+
+    public String getFristName() {
+        return fristName;
+    }
+
+    public void setFristName(String fristName) {
+        this.fristName = fristName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
