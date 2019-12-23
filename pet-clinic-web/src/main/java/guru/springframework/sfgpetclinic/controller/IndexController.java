cd21197dc83b06469b09ecf5a771ab5b3ef99449
Stiqilis <sarrenabbleoos@gmail.com>
2019-12-23 09:27:00 +0100

    Adding index page and controller. closes #8

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/IndexController.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    // Bij een request naar de root context zal dit overeenkomen met de string lijst in de requestmapping
+    // When starting spring and yoor page isn't going up look for the requestmapping handler to know the issue
+    @RequestMapping({"", "/", "index", "index.html"})
+
+    // with the return "index" the index.html page will be found
+    public String index(){
+
+        return "index";
+    }
+}
